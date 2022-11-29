cf7593e37b5eee29f1919cb9b30723f4b513e41e
Andy McBeath <mcbeath1@gmail.com>
2022-11-29 11:12:50 -0600

    Added Edit User to top and button

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
   <div class="container text-center">
     <div class="row">
       <div class="col">1 of 3</div>
-      <div class="col-6">2 of 3 (wider)</div>
+      <div class="col-6">
+        2 of 3 (wider)
+        <h3>Edit User</h3>
+        <button type="button" class="btn btn-outline-primary">Primary</button>
+      </div>
+
       <div class="col">3 of 3</div>
     </div>
     <div class="row">
